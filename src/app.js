d60b3e38056d00f5a491e2fb55f16dbb3b5268a6
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-02-16 17:18:17 -0800

    Setup emit function

--- src/app.js
+++ src/app.js
@@ -1,20 +1,18 @@
 "use strict";
 
 module.exports = function(socket, config) {
+  var emit = function() {
+    var args = Array.prototype.slice.call(arguments, 0);
+    console.log("emitting", args);
+    socket.write({args:args});
+  };
+
   socket.on('relay error', function(data) {
     console.log("relay error", data);
   });
 
-
   socket.on('please identify', function() {
-
-    socket.write({
-      args: ["i am a netpocketos device", config.token, {
-        capabilities: [],
-        status: {
-          idle: true
-        }
-      }]
-    });
+    console.log("relay is asking for identification");
+    emit("i am a netpocketos device", config.token, {});
   });
 };
