cd830a290a070a6bb4559e0d87a9f1206288f7e4
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-02-25 06:06:40 -0800

    add some more os probing

--- opt/device/features/os/manifest.js
+++ opt/device/features/os/manifest.js
@@ -3,6 +3,25 @@
 var os = require('os');
 
 module.exports = {
+  'get info': {
+    fn: function(cb) {
+      cb(null, {
+        type: os.type(),
+        platform: os.platform(),
+        architecture: os.arch(),
+        release: os.release(),
+        cpus: os.cpus()
+      });
+    }
+  },
+  'get memory stats': {
+    fn: function(cb) {
+      cb(null, {
+        total: os.totalmem(),
+        free: os.freemem()
+      });
+    }
+  },
   'get uptime': {
     fn: function(cb) {
       cb(null, os.uptime());
