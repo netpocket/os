d863c0726ed5b39b8543425ad86d3036c1df2a6a
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-02-27 18:44:40 -0800

    assert cam needs raspistill

--- test/opt/device/features/camera_spec.js
+++ test/opt/device/features/camera_spec.js
@@ -1,7 +1,14 @@
 require('../../../spec_helper.js');
-require('../../../../opt/device/features/camera/manifest.js');
+var pkg =require('../../../../opt/device/features/camera/manifest.js');
+
+var fs = require('fs');
 
 describe("Feature: camera", function () {
   describe("get still", function() {
+    if (pi) {
+      it("requires raspistill", function() {
+        expect(fs.existsSync('/opt/vc/bin/raspistill')).to.eq(true);
+      });
+    }
   });
 });
