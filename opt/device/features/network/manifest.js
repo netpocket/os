202e15d4dff921483e4100764cfa7419f7b5d298
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-03-01 01:23:06 -0800

    add wifi scanning feature

--- opt/device/features/network/manifest.js
+++ opt/device/features/network/manifest.js
@@ -1,17 +1,38 @@
 /* Provides some network related stuff */
 
 var os = require('os');
+var exec = require('child_process').exec;
 
-module.exports = {
-  'get hostname': {
-    fn: function(cb) {
-      cb(null, os.hostname());
+module.exports = function(device) {
+  return {
+    'scan wifi': {
+      fn: function(cb) {
+        exec('iwlist wlan0 scan', function(err, stdout, stderr){
+          if (err !== null) {
+            cb({
+              stderr: stderr,
+              message: err.message,
+              stack: err.stack
+            }, null);
+          } else {
+            cb(null, {
+              contentType: 'text/plain',
+              content: stdout
+            });
+          }
+        });
+      }
+    },
+    'get hostname': {
+      fn: function(cb) {
+        cb(null, os.hostname());
+      }
+    },
+    'get interfaces': {
+      fn: function(cb) {
+        cb(null, os.networkInterfaces());
+      }
     }
-  },
-  'get interfaces': {
-    fn: function(cb) {
-      cb(null, os.networkInterfaces());
-    }
-  }
+  };
 };
 
