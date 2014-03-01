55acf7ad69d2465a0d714077581a5e779fc5eace
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-03-01 05:40:51 -0800

    check in rgb matrix printer

--- opt/device/features/camera/rgbMatrix.js
+++ opt/device/features/camera/rgbMatrix.js
@@ -7,36 +7,72 @@
 // official docs about it
 // http://nodejs.org/api/stream.html#stream_class_stream_readable
 
-var fs = require('fs');
-var Buffer = require('buffer').Buffer;
-var constants = require('constants');
+// more jump off points
+// http://stackoverflow.com/questions/1423925/changing-rgb-color-values-to-represent-a-value
+// http://polymathprogrammer.com/2008/08/04/basic-colour-theory-for-programmers/
+// http://en.wikipedia.org/wiki/RGB#Numeric_representations
 
+var fs = require('fs');
 var file = process.argv[2];
-var width = process.argv[3];
-var height = process.argv[4];
+var width = parseInt(process.argv[3]);
+var height = parseInt(process.argv[4]);
 
 if (!height) {
-  console.error("  Usage: node <script> <a yuv420p file> <width> <height>");
+  console.error("  Usage: node <script> <an rgb888 file> <width> <height>");
   process.exit(1);
 }
+
+var time = new Date();
 
 var readStream = fs.createReadStream(file);
 readStream.on('readable', function() {
   var chunk;
-  while (null !== (chunk = readStream.read(1))) {
-    gotOctet(chunk[0]);
+  while (null !== (chunk = readStream.read(3))) {
+    // RGB enters as raw byte data in order of RGBRGBRGB
+    // The pixel order is from left to right and from top to bottom.
+    // This octet will first be R
+    // then G
+    // then B
+    // and so on for as long as the width
+    // then we will see the same... over and over until we've gone through height
+    var r = chunk.readUInt8(0);
+    var g = chunk.readUInt8(1);
+    var b = chunk.readUInt8(2);
+    showMatrix(r,g,b);
   }
 }).on('end', function() {
-  console.log("Done reading");
+  console.log("Done reading in %d seconds", ((new Date()) - time) / 1000);
 });
 
-var max = 0;
+// we start at the top-left
+// var row = 1;
+var col = 1;
 
+function pad(i) {
+  return ("000" + i).slice(-3);
+}
 
+console.log("Width: "+ width+ "\nHeight: "+ height);
 
-var gotOctet = function(octet) {
-  if (octet > max) {
-    max = octet;
-    console.log(octet);
+var showMatrix = function(r,g,b) { // R G B as integers from 0 to 255
+  // Print triplet
+  process.stdout.write("("+pad(r)+','+pad(g)+','+pad(b)+')');
+  // process.stdout.write(pad(col));
+
+  // We've gone through all columns if our column count == the width ...
+  if (col === width) {
+    // Print a newline
+    process.stdout.write('\n');
+    // process.stdout.write('End of row '+row+'\n');
+
+    // Reset the column counter
+    col = 1;
+
+    // Track the row too
+    // row++;
+  } else {
+    // Print a comma
+    process.stdout.write(',');
+    ++col; // We handled an RGB triplet, so increment column counter
   }
 };
