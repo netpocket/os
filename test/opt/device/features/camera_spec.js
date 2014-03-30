cc9adf3b0a373fdfb4a894ac5da38b21beb5aa83
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-03-30 01:40:36 -0700

    add a camera test

--- test/opt/device/features/camera_spec.js
+++ test/opt/device/features/camera_spec.js
@@ -1,6 +1,8 @@
 require('../../../spec_helper.js');
 var pkg =require('../../../../opt/device/features/camera/manifest.js');
 
+var PiCamera = require('../../../../opt/device/features/camera/models/pi_camera.js');
+
 var fs = require('fs');
 
 describe("Feature: camera", function () {
@@ -11,6 +13,24 @@ describe("Feature: camera", function () {
       });
       it("requires raspiyuv", function() {
         expect(fs.existsSync('/opt/vc/bin/raspiyuv')).to.eq(true);
+      });
+    });
+
+    describe("PiCamera", function() {
+      var camera = null;
+      beforeEach(function() {
+        camera = new PiCamera();
+      });
+      describe("getStill", function() {
+        it("outputs image data", function(done) {
+          camera.arm(function() {
+            camera.getStill(function(err, res) {
+              expect(err).to.eq(null);
+              expect(res.content.length > 0).to.be.true;
+              done();
+            });
+          });
+        });
       });
     });
   });
