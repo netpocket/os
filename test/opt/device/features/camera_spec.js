d49e6b3701eddd07cad2271f042f50395d86d3e0
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-03-30 04:19:00 -0700

    move pi camera specs out into their own area

--- test/opt/device/features/camera_spec.js
+++ test/opt/device/features/camera_spec.js
@@ -15,28 +15,5 @@ describe("Feature: camera", function () {
         expect(fs.existsSync('/opt/vc/bin/raspiyuv')).to.eq(true);
       });
     });
-
-    describe("Model: PiCamera", function() {
-      var camera = null;
-      beforeEach(function(done) {
-        camera = new PiCamera();
-        camera.arm(done);
-      });
-      afterEach(function(done) {
-        camera.disarm(done);
-      });
-      describe.only("getStill()", function() {
-        it("returns a readable stream (for a jpeg)", function(done) {
-          this.timeout(10000);
-          camera.getStill(function(err, res) {
-            expect(err).to.eq(null);
-            res.on('readable', function() {
-              expect(res.read(1).length).to.eq(1);
-              done();
-            });
-          });
-        });
-      });
-    });
   });
 });
