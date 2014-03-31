042e1aaf9eb32539b261a77557ca0cea0ae154b9
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-03-30 20:10:07 -0700

    add loadAttributes and persistAttributes to device

--- test/src/models/device_spec.js
+++ test/src/models/device_spec.js
@@ -1,6 +1,7 @@
 require('../../spec_helper.js');
 var Device = require('../../../src/models/device.js');
 var Backbone = require('backbone');
+var fs = require('fs');
 
 describe("Device", function() {
   var device = null,
@@ -45,6 +46,34 @@ describe("Device", function() {
         features.os['get uptime'].fn(spy);
         expect(spy).to.have.been.calledWith(null, '12345');
       });
+    });
+  });
+
+  describe("loadAttributes()", function() {
+    it("loads attributes from disk synchronously", function(done) {
+      device.configFile = __dirname+'/../../fixtures/device.json';
+      fs.writeFile(device.configFile, JSON.stringify({name:'The Name'}), function() {
+        device.set('name', 'foo');
+        device.loadAttributes();
+        expect(device.get('name')).to.eq('The Name');
+        done();
+      });
+    });
+  });
+
+  describe("persistAttributes()", function() {
+    it("writes attributes to disk asynchronously", function(done) {
+      device.configFile = __dirname+'/../../fixtures/device_tmp.json';
+      fs.writeFile(device.configFile, JSON.stringify({name:''}), function() {
+        device.set('name', 'persist test');
+        device.persistAttributes(function() {
+          expect(require(device.configFile).name).to.eq('persist test');
+          done();
+        });
+      });
+    });
+    afterEach(function() {
+      fs.unlink(device.configFile);
     });
   });
 });
